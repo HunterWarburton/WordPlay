f773f8ba44a652e868919d0d6d218bca589624ce
HunterWarburton <warburtonhunter@gmail.com>
2023-01-01 15:04:36 -0700

    Update anagrams.js

--- anagrams.js
+++ anagrams.js
@@ -1,19 +1,22 @@
 function calculateAnagrams(word) {
-  // Base case: if the word is empty, there are no anagrams
-  if (word.length === 0) {
-    return [];
-  }
-  
+	console.log("calculateAnagrams");
+// code to calculate anagrams of the word
+var anagrams = [];
+// base case
+if (word.length === 1) {
+anagrams.push(word);
+return anagrams;
+}
   // Recursive case: calculate the anagrams of the word by generating
   // all the possible permutations of the characters in the word and
   // checking which ones are valid English words
-  const anagrams = [];
-  for (let i = 0; i < word.length; i++) {
-    const remainingChars = word.substring(0, i) + word.substring(i + 1);
-    const subAnagrams = calculateAnagrams(remainingChars);
-    for (const subAnagram of subAnagrams) {
-      anagrams.push(word[i] + subAnagram);
-    }
-  }
-  return anagrams;
+for (var i = 0; i < word.length; i++) {
+var letter = word[i];
+var remainingLetters = word.substring(0, i) + word.substring(i + 1);
+var subAnagrams = calculateAnagrams(remainingLetters);
+subAnagrams.forEach(function(subAnagram) {
+anagrams.push(letter + subAnagram);
+});
+}
+return anagrams;
 }
