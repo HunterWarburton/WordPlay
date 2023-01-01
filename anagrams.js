d4be759afc398d686c2365d0eff18801e1201f58
HunterWarburton <warburtonhunter@gmail.com>
2023-01-01 14:46:23 -0700

    Update anagrams.js
    rewrote code to work with html page

--- anagrams.js
+++ anagrams.js
@@ -1,33 +1,19 @@
-function getAnagrams(word) {
-  // base case: if the word is empty, there are no anagrams
+function calculateAnagrams(word) {
+  // Base case: if the word is empty, there are no anagrams
   if (word.length === 0) {
     return [];
   }
-
-  // recursive case: if the word has at least one character,
-  // find all of the anagrams of the word without the first character
-  // and add the first character to each of those anagrams
-  let anagrams = getAnagrams(word.slice(1));
-  let firstChar = word[0];
-  let newAnagrams = [];
-
-  // loop through each of the existing anagrams
-  for (let i = 0; i < anagrams.length; i++) {
-    // insert the first character into every possible position in the anagram
-    for (let j = 0; j <= anagrams[i].length; j++) {
-      newAnagrams.push(
-        anagrams[i].slice(0, j) + firstChar + anagrams[i].slice(j)
-      );
+  
+  // Recursive case: calculate the anagrams of the word by generating
+  // all the possible permutations of the characters in the word and
+  // checking which ones are valid English words
+  const anagrams = [];
+  for (let i = 0; i < word.length; i++) {
+    const remainingChars = word.substring(0, i) + word.substring(i + 1);
+    const subAnagrams = calculateAnagrams(remainingChars);
+    for (const subAnagram of subAnagrams) {
+      anagrams.push(word[i] + subAnagram);
     }
   }
-
-  // if there were no existing anagrams, the only anagram is the word itself
-  if (anagrams.length === 0) {
-    newAnagrams.push(firstChar);
-  }
-
-  return newAnagrams;
+  return anagrams;
 }
-
-// test the function
-console.log(getAnagrams("cat"));  // should print ["cat", "cta", "act", "atc", "tca", "tac"]
